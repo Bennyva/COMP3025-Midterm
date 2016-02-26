40c02e53ef76e2712a62569100de10bae8b82771
student <student@students-mac.student.georgianc.on.ca>
2016-02-26 10:50:36 -0500

    Code behind

--- ShoppingList/ViewController.swift
+++ ShoppingList/ViewController.swift
@@ -9,17 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var listOneCount: UILabel!
+    @IBOutlet weak var listTwoCount: UILabel!
+    @IBOutlet weak var listThreeCount: UILabel!
+    @IBOutlet weak var listFourCount: UILabel!
+    @IBOutlet weak var listFiveCount: UILabel!
+    
+    @IBOutlet weak var shoppingList: UITextField!
+    @IBOutlet weak var listOneLabel: UITextField!
+    @IBOutlet weak var listTwoLabel: UITextField!
+    @IBOutlet weak var listThreeLabel: UITextField!
+    @IBOutlet weak var listFourLabel: UITextField!
+    @IBOutlet weak var listFiveLabel: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+
+    @IBAction func incrementUp(sender: AnyObject) {
+        
     }
 
-
+    @IBAction func incrementDown(sender: AnyObject) {
+    }
+    @IBAction func clearForm(sender: AnyObject) {
+        shoppingList.text = "Shopping List"
+        listOneLabel.text = "Enter List Item"
+        listTwoLabel.text = "Enter List Item"
+        listThreeLabel.text = "Enter List Item"
+        listFourLabel.text = "Enter List Item"
+        listFiveLabel.text = "Enter List Item"
+        
+        listOneCount.text = "0"
+        listTwoCount.text = "0"
+        listThreeCount.text = "0"
+        listFourCount.text = "0"
+        listFiveCount.text = "0"
+    }
 }
 
